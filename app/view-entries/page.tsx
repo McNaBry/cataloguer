78dff4e7e1ce6b61c482b5be935eeff1454499b9
McNaBry <mcnabry123@gmail.com>
2023-12-10 14:13:00 +0800

    Add page to view entries

---
+++ app/view-entries/page.tsx
@@ -0,0 +1,7 @@
+export default function EntryViewer() {
+    return (
+      <main className="h-full p-24 flex flex-col items-center content-center">
+        <h1 className="font-emerl text-5xl mb-2">Category 1</h1>
+      </main>
+    )
+}
