25c7c7e747b32ba9625c2512dae8f92a018159c0
McNaBry <mcnabry123@gmail.com>
2023-12-11 23:36:01 +0800

    Modify entry div to navigate to entry page

--- app/view-entries/page.tsx
+++ app/view-entries/page.tsx
@@ -1,6 +1,7 @@
 "use client"
 
-import { useSearchParams } from 'next/navigation'
+import { AppRouterInstance } from 'next/dist/shared/lib/app-router-context.shared-runtime';
+import { useSearchParams, useRouter } from 'next/navigation'
 
 type Entry = {
   id: string;
@@ -14,9 +15,14 @@ const entries: Entry[] = [
   {id: "003", title: "Overfloooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooow", description: "Overfloooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooow."}
 ]
 
-function EntryCard({ entry }: { entry: Entry }) {
+function EntryCard({ entry, router }: { entry: Entry, router: AppRouterInstance }) {
+  function navigateToEntryPage() {
+    router.push("/entry")
+  }
+  
   return (
-    <div className="w-full h-fit py-4 px-3 my-2 rounded cursor-default flex flex-col justify-center hover:bg-brown-rose">
+    <div className="w-full h-fit py-4 px-3 my-2 rounded cursor-default flex flex-col justify-center hover:bg-brown-rose"
+      onClick={navigateToEntryPage}>
       <span className="w-fit max-w-full bg-narvik py-1 px-2 mb-1.5">
         <p className="text-xl text-black text-ellipsis overflow-hidden">{entry.title}</p>
       </span>
@@ -26,12 +32,13 @@ function EntryCard({ entry }: { entry: Entry }) {
 }
 
 function EntryList() {
+  const router = useRouter()
   return (
     <div className="w-full lg:w-1/2 p-2 flex flex-col">
       {entries.map((entry, indx) => {
         return (
           <div key={entry.id}>
-            <EntryCard entry={entry} />
+            <EntryCard entry={entry} router={router} />
             {indx < entries.length - 1 
               ? <hr className="h-0.5 bg-narvik"/>
               : null 
